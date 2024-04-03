cf8c9ab822e2a4f7b1df1564f0da758d949ece79
Jerry21082-alt <123124690+Jerry21082-alt@users.noreply.github.com>
2024-04-03 16:53:19 +0100

    initial commit

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -15,7 +15,7 @@ export default function ProductList({ product }) {
       className={`h-full cursor-pointer group/main`}
     >
       <div className="flex flex-col w-full group">
-        <div className="relative group bg-cardBg overflow-hidden rounded-xl aspect-square p-4">
+        <div className="relative group bg-cardBg overflow-hidden rounded-xl aspect-square p-4 h-auto w-full">
           <Image
             src={product.product_photo}
             alt="product image"
@@ -24,7 +24,21 @@ export default function ProductList({ product }) {
             height={500}
             className="z-10 h-full w-full"
           />
+
+          <svg
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 28 28"
+            className="absolute top-4 right-4"
+            fill="#fff"
+          >
+            <title>heart</title>
+            <path d="M14 26c-0.25 0-0.5-0.094-0.688-0.281l-9.75-9.406c-0.125-0.109-3.563-3.25-3.563-7 0-4.578 2.797-7.313 7.469-7.313 2.734 0 5.297 2.156 6.531 3.375 1.234-1.219 3.797-3.375 6.531-3.375 4.672 0 7.469 2.734 7.469 7.313 0 3.75-3.437 6.891-3.578 7.031l-9.734 9.375c-0.187 0.187-0.438 0.281-0.688 0.281z"></path>
+          </svg>
         </div>
+
         <h3 className="mt-4 font-medium text-sm">
           {product.product_description.length > 25
             ? product.product_description.substring(0, 25).concat("...")
