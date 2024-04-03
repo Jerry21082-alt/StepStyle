ad3f677da9b143596b7317c6e14047e2f9b3af3c
Jerry21082-alt <123124690+Jerry21082-alt@users.noreply.github.com>
2024-04-03 14:01:11 +0100

    initial commit

--- components/ImageSlider.jsx
+++ components/ImageSlider.jsx
@@ -5,21 +5,22 @@ import "swiper/css/pagination";
 
 export default function ImageSlider({ url }) {
   return (
-    <div
-      className="relative group aspect-square
-   bg-zinc-100 overflow-hidden rounded-xl"
-    >
-      <Swiper className="w-full h-full">
-        <SwiperSlide className="relative -z-10 h-full w-full">
-          <Image
-            src={`/${url}`}
-            alt="product image"
-            fill
-            loading="eager"
-            className="-z-10 h-full w-full object-cover object-center"
-          />
-        </SwiperSlide>
-      </Swiper>
-    </div>
+    //   <div
+    //     className="relative group aspect-square
+    //  bg-zinc-100 overflow-hidden rounded-xl"
+    //   >
+    //     <Swiper className="w-full h-full">
+    //       <SwiperSlide className="relative -z-10 h-full w-full">
+    //         <Image
+    //           src={`/${url}`}
+    //           alt="product image"
+    //           fill
+    //           loading="eager"
+    //           className="-z-10 h-full w-full object-cover object-center"
+    //         />
+    //       </SwiperSlide>
+    //     </Swiper>
+    //   </div>
+    <Image src='pngegg (1).png'/>
   );
 }
