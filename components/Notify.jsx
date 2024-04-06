f437ba0608fe333c96ed7e9c4eef108f64cc8896
Jerry21082-alt <123124690+Jerry21082-alt@users.noreply.github.com>
2024-04-06 18:08:07 +0100

    initial commit

--- components/Notify.jsx
+++ components/Notify.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { stateFunc } from "./stateContent/UseStateContext";
 
 export default function Notify() {
