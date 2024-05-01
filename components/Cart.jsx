6c5f9f246b46beb30f812bbc403cf6372bc94eee
Jerry21082-alt <123124690+Jerry21082-alt@users.noreply.github.com>
2024-05-01 14:49:58 +0100

    initial commit

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -48,50 +48,47 @@ export default function Cart() {
       }`}
     >
       <button
-        className={`flex justify-center gap-2 items-center outline-none border-none bg-secondaryColor py-2 px-4 rounded-2xl text-sm md:m-2 ${
+        className={`flex items-center space-x-2 absolute top-2 left-1  bg-buttonColor rounded-md p-1 ${
           !toggleCart ? "animate-cart-btn" : "static-cart-btn"
         }`}
         onClick={() => setToggleCart(false)}
       >
-        <svg
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          width="15"
-          height="20"
-          viewBox="0 0 20 20"
-          fill="#fff"
-        >
-          <title>arrow-long-left</title>
-          <path d="M0.75 10l5.25-5.5v3.5h13v4h-13v3.5l-5.25-5.5z"></path>
-        </svg>{" "}
-        <span className="text-snow font-bold">Your</span>
+        <div className="w-5 h-5">
+          <svg
+            viewBox="0 0 24 24"
+            fill="#fff"
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-full h-full"
+          >
+            <g id="Arrow / Chevron_Left_MD">
+              <path id="Vector" d="M14 16L10 12L14 8" />
+            </g>
+          </svg>
+        </div>
+        <p className="text-snow">Your</p>
         {isMounted ? (
-          <span className="text-snow font-bold">
+          <p className="text-snow">
             {cartItems.length <= 1 ? "item" : "items"}
-          </span>
+          </p>
         ) : null}
       </button>
       {isMounted && !cartItems.length ? (
         <div className="flex justify-center items-center h-[70vh]">
           <div className="flex flex-col items-center">
-            <svg
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
-              width="84"
-              height="84"
-              viewBox="0 0 24 24"
-              onClick={() => setToggleCart(true)}
-            >
-              <title>shopping-cart</title>
-              <path d="M20.756 5.345c-0.191-0.219-0.466-0.345-0.756-0.345h-13.819l-0.195-1.164c-0.080-0.482-0.497-0.836-0.986-0.836h-2.25c-0.553 0-1 0.447-1 1s0.447 1 1 1h1.403l1.86 11.164c0.008 0.045 0.031 0.082 0.045 0.124 0.016 0.053 0.029 0.103 0.054 0.151 0.032 0.066 0.075 0.122 0.12 0.179 0.031 0.039 0.059 0.078 0.095 0.112 0.058 0.054 0.125 0.092 0.193 0.13 0.038 0.021 0.071 0.049 0.112 0.065 0.116 0.047 0.238 0.075 0.367 0.075 0.001 0 11.001 0 11.001 0 0.553 0 1-0.447 1-1s-0.447-1-1-1h-10.153l-0.166-1h11.319c0.498 0 0.92-0.366 0.99-0.858l1-7c0.041-0.288-0.045-0.579-0.234-0.797zM18.847 7l-0.285 2h-3.562v-2h3.847zM14 7v2h-3v-2h3zM14 10v2h-3v-2h3zM10 7v2h-3c-0.053 0-0.101 0.015-0.148 0.030l-0.338-2.030h3.486zM7.014 10h2.986v2h-2.653l-0.333-2zM15 12v-2h3.418l-0.285 2h-3.133z"></path>
-              <path d="M10 19.5c0 0.828-0.672 1.5-1.5 1.5s-1.5-0.672-1.5-1.5c0-0.828 0.672-1.5 1.5-1.5s1.5 0.672 1.5 1.5z"></path>
-              <path d="M19 19.5c0 0.828-0.672 1.5-1.5 1.5s-1.5-0.672-1.5-1.5c0-0.828 0.672-1.5 1.5-1.5s1.5 0.672 1.5 1.5z"></path>
-            </svg>
-
-            <p className="text-xl font-bold my-2">Your Cart is Empty</p>
+            <div className="w-32 h-32">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                id="shopping-bag"
+                className="w-full h-full"
+              >
+                <path d="M20,7.85A1,1,0,0,0,19,7H17A5,5,0,0,0,7,7H5a1,1,0,0,0-1,.85l-2,13A1,1,0,0,0,3,22H21a1,1,0,0,0,1-1.15ZM12,4a3,3,0,0,1,3,3H9A3,3,0,0,1,12,4Z"></path>
+              </svg>
+            </div>
+            <p className="text-xl font-bold my-2">Your Bag is Empty</p>
             <button
               onClick={() => setToggleCart(false)}
-              className="bg-secondaryColor rounded-3xl text-snow mt-5 py-2 px-4"
+              className="bg-black rounded-3xl text-snow mt-5 py-2 px-4"
             >
               <Link href={`/`}>Continue shopping</Link>
             </button>
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -14,11 +14,10 @@ export default function Hero() {
   }, [ref]);
 
   return (
-    // <AspectRatioContainer className={`hero-section`} aspectRatio={1/ 1}>
     <section className="hero-section h-[60vh] md:h-[70vh]" ref={ref}>
       <div className="absolute top-1/4 left-0 right-12 bottom-0 z-20 p-5 h-full">
         <h1 className="text-snow text-3xl">
-          StepStyle: Your Ultimate Destination for Sneakerheads
+          <span className="text-secondaryColor">StepStyle:</span> Your Ultimate Destination for Sneakerheads
         </h1>
 
         <button className="mt-12 border-2 border-snow py-1 px-4 rounded-3xl text-snow">
